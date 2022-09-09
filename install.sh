cbb1803b2957ee79595c6e785f62ee0548035eae
tvrcgo <tvrcgo@gmail.com>
2022-09-09 15:12:09 +0800

    use embeded compose

--- install.sh
+++ install.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 
-# # install docker-compose
-# curl -L https://github.com/docker/compose/releases/download/1.8.0/run.sh > /usr/local/bin/docker-compose
-# chmod +x /usr/local/bin/docker-compose
-# docker-compose --version
-
 # compose
 docker compose up --build -d
