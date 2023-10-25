287b99a1f18075ebe5bf640bb3afeced1c43f70e
tvrcgo <tvrcgo@gmail.com>
2023-10-25 16:36:16 +0800

    fix acme-runner

--- acme-runner/acme.sh
+++ acme-runner/acme.sh
@@ -1,23 +1,6 @@
 #!/bin/sh
 
 source /root/.env
-curl https://get.acme.sh | sh;
-
-case $DNS_SVC in
-  dns_gd)
-    export GD_Key="$DNS_AK"
-    export GD_Secret="$DNS_SK"
-    ;;
-  dns_ali)
-    export Ali_Key="$DNS_AK"
-    export Ali_Secret="$DNS_SK"
-    ;;
-  dns_dp)
-    export DP_Id="$DNS_AK"
-    export DP_Key="$DNS_SK"
-    ;;
-  *) ;;
-esac
 
 MAIL="admin@$DOMAIN"
 
